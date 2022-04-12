8df1716aa2e0645fa71db5caf4fc1a9348df4ae9
Atsushi Nakamura <nuits.jp@live.jp>
2022-04-13 06:11:29 +0900

    Add KamishibaiApp.

---
+++ Source/KamishibaiApp.App/Program.cs
@@ -0,0 +1,8 @@
+﻿using KamishibaiApp.View;
+
+// Create a builder by specifying the application and main window.
+var builder = WpfApplication<App, MainWindow>.CreateBuilder(args);
+
+// Build and run the application.
+var app = builder.Build();
+app.RunAsync();
