24d3b1d9bd445fdeb2f3f63fe45e064062bf863f
Atsushi Nakamura <nuits.jp@live.jp>
2022-04-19 05:09:01 +0900

    Implement Navigate by Type.

---
+++ Sample/SampleBrowser/SampleBrowser.View/Page/DefaultConstructorPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls ;
+
+namespace SampleBrowser.View.Page ;
+
+public partial class DefaultConstructorPage : UserControl
+{
+  public DefaultConstructorPage()
+  {
+    InitializeComponent() ;
+  }
+}
