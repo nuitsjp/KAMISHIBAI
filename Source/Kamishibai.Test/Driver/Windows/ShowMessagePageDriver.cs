3ee17dde7d986c3d260501a34396bc42a2b5a59d
Atsushi Nakamura <nuits.jp@live.jp>
2022-04-29 07:04:24 +0900

    Add ShowMessageTest.

---
+++ Source/Kamishibai.Test/Driver/Windows/ShowMessagePageDriver.cs
@@ -0,0 +1,43 @@
+﻿using Codeer.Friendly;
+using Codeer.Friendly.Dynamic;
+using Codeer.Friendly.Windows;
+using Codeer.Friendly.Windows.Grasp;
+using Codeer.TestAssistant.GeneratorToolKit;
+using RM.Friendly.WPFStandardControls;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace Driver.Windows
+{
+    [WindowDriver(TypeFullName = "SampleBrowser.View.MainWindow")]
+    public class ShowMessagePageDriver
+    {
+        public WindowControl Core { get; }
+        public WPFTextBox Message => Core.LogicalTree().ByType<ContentControl>().ByContentText("System.Windows.Controls.Grid").Single().LogicalTree().ByBinding("Message").FirstOrDefault()?.Dynamic(); 
+        public WPFContextMenu MessageContextMenu => new WPFContextMenu{Target = Message.AppVar};
+        public WPFTextBox Caption => Core.LogicalTree().ByType<ContentControl>().ByContentText("System.Windows.Controls.Grid").Single().LogicalTree().ByBinding("Caption").FirstOrDefault()?.Dynamic(); 
+        public WPFContextMenu CaptionContextMenu => new WPFContextMenu{Target = Caption.AppVar};
+        public WPFComboBox SelectedMessageBoxButton => Core.LogicalTree().ByType<ContentControl>().ByContentText("System.Windows.Controls.Grid").Single().LogicalTree().ByBinding("SelectedMessageBoxButton").FirstOrDefault()?.Dynamic(); 
+        public WPFComboBox SelectedMessageBoxImage => Core.LogicalTree().ByType<ContentControl>().ByContentText("System.Windows.Controls.Grid").Single().LogicalTree().ByBinding("SelectedMessageBoxImage").FirstOrDefault()?.Dynamic(); 
+        public WPFComboBox SelectedMessageBoxResult => Core.LogicalTree().ByType<ContentControl>().ByContentText("System.Windows.Controls.Grid").Single().LogicalTree().ByBinding("SelectedMessageBoxResult").FirstOrDefault()?.Dynamic(); 
+        public WPFComboBox SelectedMessageBoxOptions => Core.LogicalTree().ByType<ContentControl>().ByContentText("System.Windows.Controls.Grid").Single().LogicalTree().ByBinding("SelectedMessageBoxOptions").FirstOrDefault()?.Dynamic(); 
+        public WPFButtonBase ShowMessageCommand => Core.LogicalTree().ByType<ContentControl>().ByContentText("System.Windows.Controls.Grid").Single().LogicalTree().ByBinding("ShowMessageCommand").FirstOrDefault()?.Dynamic(); 
+
+        public ShowMessagePageDriver(WindowControl core)
+        {
+            Core = core;
+        }
+
+        public ShowMessagePageDriver(AppVar core)
+        {
+            Core = new WindowControl(core);
+        }
+    }
+
+    public static class ShowMessagePageDriverExtensions
+    {
+        [WindowDriverIdentify(TypeFullName = "SampleBrowser.View.MainWindow")]
+        public static ShowMessagePageDriver AttachShowMessagePage(this WindowsAppFriend app)
+            => app.WaitForIdentifyFromTypeFullName("SampleBrowser.View.MainWindow").Dynamic();
+    }
+}
---
+++ Source/Kamishibai.Test/Scenario/ShowMessageTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using Codeer.Friendly;
+using Driver.Windows;
+using Driver.Windows.Native;
+using FluentAssertions;
+
+namespace Scenario;
+
+public class ShowMessageTest : TestBase
+{
+    [Test]
+    public void Show()
+    {
+        var mainWindow = app.AttachMainWindow();
+        var showMessagePage = app.AttachShowMessagePage();
+        // Navigate show message
+        mainWindow.SelectedMenuItem.EmulateChangeSelectedIndex(3);
+
+        // Setup dialog 
+        showMessagePage.Message.EmulateChangeText("Hello, Message!");
+        showMessagePage.Caption.EmulateChangeText("Hello, Caption!");
+        showMessagePage.SelectedMessageBoxButton.EmulateChangeSelectedIndex(1);
+        showMessagePage.SelectedMessageBoxImage.EmulateChangeSelectedIndex(2);
+        showMessagePage.SelectedMessageBoxResult.EmulateChangeSelectedIndex(3);
+
+        // Show message dialog
+        var async = new Async();
+        showMessagePage.ShowMessageCommand.EmulateClick(async);
+        var messageBox = app.Attach_MessageBox(@"Hello, Caption!");
+        messageBox.Button_OK.Enabled.Should().BeTrue();
+        messageBox.Button_Cancel.Enabled.Should().BeTrue();
+
+        // Cancel dialog
+        messageBox.Button_Cancel.EmulateClick();
+        async.WaitForCompletion();
+
+    }
+}
