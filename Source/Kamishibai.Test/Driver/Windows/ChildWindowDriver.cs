f5f3001c3a6e50f0054c5f95f381233a4ef69fa3
Atsushi Nakamura <nuits.jp@live.jp>
2022-04-29 05:23:19 +0900

    Add ChildWindowDriver.

---
+++ Source/Kamishibai.Test/Driver/Windows/ChildWindowDriver.cs
@@ -0,0 +1,36 @@
+﻿using Codeer.Friendly;
+using Codeer.Friendly.Dynamic;
+using Codeer.Friendly.Windows;
+using Codeer.Friendly.Windows.Grasp;
+using Codeer.TestAssistant.GeneratorToolKit;
+using RM.Friendly.WPFStandardControls;
+using System.Linq;
+
+namespace Driver.Windows
+{
+    [WindowDriver(TypeFullName = "SampleBrowser.View.ChildWindow")]
+    public class ChildWindowDriver
+    {
+        public WindowControl Core { get; }
+        public WPFToggleButton BlockClosing => Core.LogicalTree().ByBinding("BlockClosing").FirstOrDefault()?.Dynamic(); 
+        public WPFButtonBase CloseCommand => Core.LogicalTree().ByBinding("CloseCommand").FirstOrDefault()?.Dynamic(); 
+        public WPFButtonBase CloseSpecifiedWindowCommand => Core.LogicalTree().ByBinding("CloseSpecifiedWindowCommand").FirstOrDefault()?.Dynamic(); 
+
+        public ChildWindowDriver(WindowControl core)
+        {
+            Core = core;
+        }
+
+        public ChildWindowDriver(AppVar core)
+        {
+            Core = new WindowControl(core);
+        }
+    }
+
+    public static class ChildWindowDriverExtensions
+    {
+        [WindowDriverIdentify(TypeFullName = "SampleBrowser.View.ChildWindow")]
+        public static ChildWindowDriver AttachChildWindow(this WindowsAppFriend app)
+            => app.WaitForIdentifyFromTypeFullName("SampleBrowser.View.ChildWindow").Dynamic();
+    }
+}
