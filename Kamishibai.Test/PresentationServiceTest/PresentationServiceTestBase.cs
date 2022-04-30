1e81dca20fa7f1bbcdeb79a11d956db367858b40
Atsushi Nakamura <nuits.jp@live.jp>
2022-04-30 18:27:09 +0900

    Add Should_be_navigated#With_safe_parameter.

--- Kamishibai.Test/PresentationServiceTest/PresentationServiceTestBase.cs
+++ Kamishibai.Test/PresentationServiceTest/PresentationServiceTestBase.cs
@@ -11,19 +11,11 @@ public abstract class PresentationServiceTestBase
     protected NavigationFrame NamedFrame { get; } = new(){FrameName = "Named"};
     protected NavigationFrameProvider NavigationFrameProvider { get; } = new();
     protected Mock<IWindowService> WindowService { get; } = new();
-    protected IPresentationServiceBase BuildService()
+    protected IPresentationService BuildService()
     {
         return new PresentationService(
             Services.BuildServiceProvider(),
             NavigationFrameProvider,
             WindowService.Object);
-    }
-
-    private class PresentationService : PresentationServiceBase
-    {
-        public PresentationService(IServiceProvider serviceProvider, INavigationFrameProvider navigationFrameProvider, IWindowService windowService)
-            : base(serviceProvider, navigationFrameProvider, windowService)
-        {
-        }
     }
 }
--- Kamishibai.Test/PresentationServiceTest/Should_be_navigated.cs
+++ Kamishibai.Test/PresentationServiceTest/Should_be_navigated.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using Xunit;
 
 namespace Kamishibai.Test.PresentationServiceTest;
@@ -14,7 +17,7 @@ public class Should_be_navigated : PresentationServiceTestBase
     {
         #region Setup
         Services.AddPresentation<FirstPage, FirstWithOutArgumentsViewModel>();
-        Services.AddPresentation<SecondPage, SecondWithOutViewModel>();
+        Services.AddPresentation<SecondPage, SecondWithOutArgumentsViewModel>();
         var service = BuildService();
         #endregion
 
@@ -22,7 +25,7 @@ public class Should_be_navigated : PresentationServiceTestBase
         DefaultFrame.Current.GetType().Should().Be(typeof(FirstPage));
         DefaultFrame.Count.Should().Be(1);
 
-        await service.NavigateAsync(typeof(SecondWithOutViewModel));
+        await service.NavigateAsync(typeof(SecondWithOutArgumentsViewModel));
         DefaultFrame.Current.GetType().Should().Be(typeof(SecondPage));
         DefaultFrame.Count.Should().Be(2);
     }
@@ -32,7 +35,7 @@ public class Should_be_navigated : PresentationServiceTestBase
     {
         #region Setup
         Services.AddPresentation<FirstPage, FirstWithOutArgumentsViewModel>();
-        Services.AddPresentation<SecondPage, SecondWithOutViewModel>();
+        Services.AddPresentation<SecondPage, SecondWithOutArgumentsViewModel>();
         var service = BuildService();
         #endregion
 
@@ -40,7 +43,7 @@ public class Should_be_navigated : PresentationServiceTestBase
         DefaultFrame.Current.GetType().Should().Be(typeof(FirstPage));
         DefaultFrame.Count.Should().Be(1);
 
-        await service.NavigateAsync<SecondWithOutViewModel>();
+        await service.NavigateAsync<SecondWithOutArgumentsViewModel>();
         DefaultFrame.Current.GetType().Should().Be(typeof(SecondPage));
         DefaultFrame.Count.Should().Be(2);
     }
@@ -50,7 +53,7 @@ public class Should_be_navigated : PresentationServiceTestBase
     {
         #region Setup
         Services.AddPresentation<FirstPage, FirstWithOutArgumentsViewModel>();
-        Services.AddPresentation<SecondPage, SecondWithOutViewModel>();
+        Services.AddPresentation<SecondPage, SecondWithOutArgumentsViewModel>();
         var service = BuildService();
         #endregion
 
@@ -58,15 +61,90 @@ public class Should_be_navigated : PresentationServiceTestBase
         DefaultFrame.Current.GetType().Should().Be(typeof(FirstPage));
         DefaultFrame.Count.Should().Be(1);
 
-        await service.NavigateAsync(new SecondWithOutViewModel());
+        await service.NavigateAsync(new SecondWithOutArgumentsViewModel());
         DefaultFrame.Current.GetType().Should().Be(typeof(SecondPage));
         DefaultFrame.Count.Should().Be(2);
     }
 
 
+    [WpfFact]
+    public async Task With_callback_initializer()
+    {
+        #region Setup
+        Services.AddPresentation<FirstPage, FirstWithOutArgumentsViewModel>();
+        Services.AddPresentation<SecondPage, SecondWithOutArgumentsViewModel>();
+        var service = BuildService();
+        #endregion
+
+        FirstWithOutArgumentsViewModel? firstWithOutArgumentsViewModel = null;
+        await service.NavigateAsync<FirstWithOutArgumentsViewModel>(viewModel => firstWithOutArgumentsViewModel = viewModel);
+        DefaultFrame.Current.GetType().Should().Be(typeof(FirstPage));
+        DefaultFrame.Current.DataContext.Should().Be(firstWithOutArgumentsViewModel);
+        DefaultFrame.Count.Should().Be(1);
+
+        SecondWithOutArgumentsViewModel? secondWithOutViewModel = null;
+        await service.NavigateAsync<SecondWithOutArgumentsViewModel>(viewModel => secondWithOutViewModel = viewModel);
+        DefaultFrame.Current.GetType().Should().Be(typeof(SecondPage));
+        DefaultFrame.Current.DataContext.Should().Be(secondWithOutViewModel);
+        DefaultFrame.Count.Should().Be(2);
+    }
+
+    [WpfFact]
+    public async Task With_safe_parameter()
+    {
+        var messages = new List<string>();
+        #region Setup
+        Services.AddPresentation<FirstPage, FirstWithArgumentsViewModel>();
+        Services.AddPresentation<SecondPage, SecondWithArgumentsViewModel>();
+        Services.AddTransient<IPresentationService, PresentationService>();
+        Services.AddTransient(_ => new Mock<IWindowService>().Object);
+        Services.AddTransient<IList<string>>(_ => messages);
+        var service = BuildService();
+        #endregion
+
+        int number = 100;
+        await service.NavigateToFirstWithArgumentsAsync(number, NamedFrame.FrameName);
+        NamedFrame.Current.GetType().Should().Be(typeof(FirstPage));
+        NamedFrame.Current.DataContext.As<FirstWithArgumentsViewModel>().Number.Should().Be(number);
+        NamedFrame.Current.DataContext.As<FirstWithArgumentsViewModel>().Messages.Should().BeEquivalentTo(messages);
+        NamedFrame.Count.Should().Be(1);
+
+        string message = "Hello, KAMISHIBAI!";
+        await service.NavigateToSecondWithArgumentsAsync(message, NamedFrame.FrameName);
+        NamedFrame.Current.GetType().Should().Be(typeof(SecondPage));
+        NamedFrame.Current.DataContext.As<SecondWithArgumentsViewModel>().Message.Should().Be(message);
+        NamedFrame.Count.Should().Be(2);
+    }
+
     public class FirstWithOutArgumentsViewModel { }
+
+    [Navigate]
+    public class FirstWithArgumentsViewModel
+    {
+        public FirstWithArgumentsViewModel(
+            int number, 
+            [Inject] IList<string> messages)
+        {
+            Number = number;
+            Messages = messages;
+        }
+
+        public int Number { get; }
+        public IList<string> Messages { get; }
+    }
     public class FirstPage : UserControl { }
-    public class SecondWithOutViewModel { }
+    public class SecondWithOutArgumentsViewModel { }
+
+    [Navigate]
+    public class SecondWithArgumentsViewModel
+    {
+        public SecondWithArgumentsViewModel(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; set; }
+    }
     public class SecondPage : UserControl { }
 
 }
