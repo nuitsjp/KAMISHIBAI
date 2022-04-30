0a8d0f8c549f4fc12699f25d673140521d04adb8
Atsushi Nakamura <nuits.jp@live.jp>
2022-04-30 19:48:33 +0900

    Add Should_be_go_backed.

---
+++ Kamishibai.Test/PresentationServiceTest/Should_be_go_backed.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Controls;
+using FluentAssertions;
+using Xunit;
+
+namespace Kamishibai.Test.PresentationServiceTest;
+
+// ReSharper disable once InconsistentNaming
+public class Should_be_go_backed : PresentationServiceTestBase
+{
+    [WpfFact]
+    public async Task When_call()
+    {
+        #region Setup
+        Services.AddPresentation<FirstPage, FirstViewModel>();
+        Services.AddPresentation<SecondPage, SecondViewModel>();
+        var service = BuildService();
+        #endregion
+
+        await service.NavigateAsync<FirstViewModel>();
+        await service.NavigateAsync<SecondViewModel>();
+        DefaultFrame.Current.Should().BeOfType<SecondPage>();
+        DefaultFrame.Count.Should().Be(2);
+
+        service.GetNavigationFrame().CanGoBack.Should().BeTrue();
+        await service.GoBackAsync();
+        DefaultFrame.Current.Should().BeOfType<FirstPage>();
+        DefaultFrame.Count.Should().Be(1);
+
+        service.GetNavigationFrame().CanGoBack.Should().BeFalse();
+        (await service.GoBackAsync()).Should().BeFalse();
+    }
+
+    public class FirstViewModel {}
+    public class SecondViewModel {}
+    public class FirstPage : UserControl { }
+    public class SecondPage : UserControl { }
+
+}
