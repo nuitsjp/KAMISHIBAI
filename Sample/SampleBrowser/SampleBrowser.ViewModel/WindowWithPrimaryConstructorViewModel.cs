c69510b0f73c185731fcbdb8d7398a52e280bba9
Freesia <freesia@studiofreesia.com>
2024-01-14 16:22:28 +0900

    Update LangVersion to 12 and add WindowWithPrimaryConstructorViewModel class

---
+++ Sample/SampleBrowser/SampleBrowser.ViewModel/WindowWithPrimaryConstructorViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Kamishibai;
+
+namespace SampleBrowser.ViewModel;
+
+[OpenWindow]
+public class WindowWithPrimaryConstructorViewModel(string argument, [Inject] IPresentationService presentationService)
+    : ChildViewModel(presentationService)
+{
+    private readonly string argument = argument;
+}
